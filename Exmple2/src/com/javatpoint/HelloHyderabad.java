b79d404e7c249d9ba397c67252c440566544909d
praveenaruna <praveen.lvr7@gmail.com>
2018-08-18 21:58:39 +0530

    First commit

---
+++ Exmple2/src/com/javatpoint/HelloHyderabad.java
@@ -0,0 +1,15 @@
+package com.javatpoint;
+
+public class HelloHyderabad {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Hyderabad");
+		System.out.println("I am just trying into GitHub wheather working or not");
+	}
+
+}
